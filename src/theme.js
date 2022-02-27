af8521af16b00dce5835b37222ddc89a85e1b6e6
targonskii <polarpando@gmail.com>
2022-02-27 12:37:04 +0300

    Installed react, webpack, jss, router

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    generalRed: '#ff5d4f',
+    black: '#000',
+    white: '#fff',
+    gray: '#616161',
+    secondaryGray: '#b5b5b5',
+    generalBlue: '#85c8ee',
+    secondaryBlue: '#d3ebf9',
+  },
+};
+
+export default theme;
